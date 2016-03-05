c9008dede1d18ef8c7df178b35dd3033f801261f
zlsun <zlsun1995@gmail.com>
2016-03-05 10:19:21 +0800

    #258

--- 258. Add Digits/solution.cpp
+++ 258. Add Digits/solution.cpp
@@ -1,6 +1,6 @@
 /** 258. Add Digits
 
-Given a non-negative integer num, repeatedly add all its digits until the result has only one digit. 
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
 For example:
 
@@ -10,7 +10,7 @@ Follow up:
 
 Could you do it without any loop/recursion in O(1) runtime?
 
-A naive implementation of the above process is trivial. Could you come up with other methods? 
+A naive implementation of the above process is trivial. Could you come up with other methods?
 What are all the possible results?
 How do they occur, periodically or randomly?
 You may find this Wikipedia article useful.
@@ -23,7 +23,24 @@ Credits:Special thanks to @jianchao.li.fighter for adding this problem and creat
 #include "../utils.h"
 using namespace std;
 
-int main() {
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
+        }
+        if (sum >= 10) return addDigits(sum);
+        return sum;
+    }
+};
 
+int main() {
+    Solution s;
+    ASSERT s.addDigits(12) == 3;
+    ASSERT s.addDigits(0) == 0;
+    ASSERT s.addDigits(1) == 1;
+    ASSERT s.addDigits(38) == 2;
     return 0;
 }
--- utils.h
+++ utils.h
@@ -1,1 +1,17 @@
 #include <zlog.h>
+
+struct Assert {
+    int lineno;
+    Assert(int l): lineno(l) {}
+    Assert& operator , (bool ok)
+    {
+        if (ok) {
+            zlog lineno, "Assert passed";
+        } else {
+            zlog lineno, "Assert failed";
+        }
+        return *this;
+    }
+};
+
+#define ASSERT Assert(__LINE__),
