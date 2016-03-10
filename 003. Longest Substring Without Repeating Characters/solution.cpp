38566e92c953d9845d36fb58c89daaf91d63b3b6
zlsun <zlsun1995@gmail.com>
2016-03-10 11:22:58 +0800

    #003

--- 003. Longest Substring Without Repeating Characters/solution.cpp
+++ 003. Longest Substring Without Repeating Characters/solution.cpp
@@ -8,7 +8,32 @@ Given a string, find the length of the longest substring without repeating chara
 #include "../utils.h"
 using namespace std;
 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        bool has[256] = {0};
+        int b = 0, e = 1, l = s.length(), m = 0;
+        if (l <= 1) return l;
+        has[s[b]] = 1;
+        while (e < l) {
+            while (has[s[e]]) {
+                has[s[b]] = 0;
+                ++b;
+            }
+            has[s[e]] = 1;
+            ++e;
+            m = std::max(m, e - b);
+        }
+        return m;
+    }
+};
+
 int main() {
     Solution s;
+    ASSERT s.lengthOfLongestSubstring("abcabcbb") == 3;
+    ASSERT s.lengthOfLongestSubstring("bbbbb") == 1;
+    ASSERT s.lengthOfLongestSubstring("b") == 1;
+    ASSERT s.lengthOfLongestSubstring("") == 0;
+    ASSERT s.lengthOfLongestSubstring("dvdf") == 3;
     return 0;
 }
