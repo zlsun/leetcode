594a09d1eb4d546d90a5d724302b424d26bf089d
zlsun <zlsun1995@gmail.com>
2016-03-12 00:28:04 +0800

    #203

--- 203. Remove Linked List Elements/solution.cpp
+++ 203. Remove Linked List Elements/solution.cpp
@@ -13,8 +13,53 @@ Credits:Special thanks to @mithmatt for adding this problem and creating all tes
 #include <iostream>
 #include "../utils.h"
 using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head && head->val == val) {
+            head = head->next;
+        }
+        if (head == NULL) return NULL;
+        ListNode* pre = head;
+        ListNode* cur = head->next;
+        while (cur) {
+            ListNode* next = cur->next;
+            if (cur->val == val) {
+                pre->next = next;
+                // delete cur;
+            } else {
+                pre = cur;
+            }
+            cur = next;
+        }
+        return head;
+    }
+};
+
+Solution s;
+
+using vl = std::vector<ListNode>;
+void test(vl l, int x, vl r) {
+    for (size_t i = 0; i < l.size() - 1; ++i) {
+        l[i].next = &l[i + 1];
+    }
+    ListNode* t = s.removeElements(&l[0], x);
+    int idx = 0;
+    while (t) {
+        ASSERT t->val == r[idx++].val;
+        t = t->next;
+    }
+}
 
 int main() {
-    Solution s;
+    test({1, 2, 6, 3, 4, 5, 6}, 6, {1, 2, 3, 4, 5});
+    test({1, 2, 1}, 1, {2});
+    test({1}, 1, {});
     return 0;
 }
