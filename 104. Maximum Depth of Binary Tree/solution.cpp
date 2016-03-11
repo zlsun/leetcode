05bb16910668cf8266a7f682ca723fd557d84b58
zlsun <zlsun1995@gmail.com>
2016-03-11 23:44:23 +0800

    #104

--- 104. Maximum Depth of Binary Tree/solution.cpp
+++ 104. Maximum Depth of Binary Tree/solution.cpp
@@ -9,7 +9,37 @@ The maximum depth is the number of nodes along the longest path from the root no
 #include "../utils.h"
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x, TreeNode* l = NULL, TreeNode* r = NULL) : val(x), left(l), right(r) {}
+    struct Builder {
+        TreeNode* p;
+        Builder(TreeNode* p = nullptr): p(p) {}
+        Builder(int d, Builder l = Builder(), Builder r = Builder())
+            : p(new TreeNode(d, l.p, r.p)) {}
+        operator TreeNode* () const {
+            return p;
+        }
+    };
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
 int main() {
     Solution s;
+    TreeNode* tree = TreeNode::Builder {
+        1,
+        { 2, {4}, {5} },
+        { 3 }
+    };
+    ASSERT s.maxDepth(tree) == 3;
     return 0;
 }
