8be71aa26a19a20a93a1f6e9eec46cb8c0de588a
zlsun <zlsun1995@gmail.com>
2016-03-12 00:13:28 +0800

    #237

--- 237. Delete Node in a Linked List/solution.cpp
+++ 237. Delete Node in a Linked List/solution.cpp
@@ -10,7 +10,39 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
 #include "../utils.h"
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* n = node->next;
+        node->val = n->val;
+        node->next = n->next;
+        // delete n;
+    }
+};
+
+Solution s;
+
+using vl = std::vector<ListNode>;
+void test(vl l, int x, vl r) {
+    for (size_t i = 0; i < l.size() - 1; ++i) {
+        l[i].next = &l[i + 1];
+    }
+    ListNode* t = &l[0];
+    s.deleteNode(&l[x]);
+    int idx = 0;
+    while (t) {
+        ASSERT t->val == r[idx++].val;
+        t = t->next;
+    }
+}
+
 int main() {
-    Solution s;
+    test({1, 2, 3, 4}, 2, {1, 2, 4});
     return 0;
 }
