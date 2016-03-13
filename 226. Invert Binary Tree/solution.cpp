88c4cff6d69d2072466835e73f2f62623f690952
zlsun <zlsun1995@gmail.com>
2016-03-14 00:09:52 +0800

    #226

--- 226. Invert Binary Tree/solution.cpp
+++ 226. Invert Binary Tree/solution.cpp
@@ -22,19 +22,21 @@ Google: 90% of our engineers use the software you wrote (Homebrew), but you canâ
 #include "../utils.h"
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
+        if (root == NULL) return root;
+        TreeNode* node = new TreeNode(root->val);
+        node->right = invertTree(root->left);
+        node->left = invertTree(root->right);
+        return node;
     }
 };
 
