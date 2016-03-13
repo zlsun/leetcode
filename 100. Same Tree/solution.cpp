855a4f32c2be2ff2975c2bab24d15fb753d031f9
zlsun <zlsun1995@gmail.com>
2016-03-14 00:09:46 +0800

    #100

--- 100. Same Tree/solution.cpp
+++ 100. Same Tree/solution.cpp
@@ -10,19 +10,18 @@ Two binary trees are considered equal if they are structurally identical and the
 #include "../utils.h"
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
+       if (p == NULL || q == NULL) return p == q;
+       return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 
