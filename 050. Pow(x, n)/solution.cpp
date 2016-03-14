7d44f7e700eecbf7039b72a4f11cd52609ba70ec
zlsun <zlsun1995@gmail.com>
2016-03-14 22:25:29 +0800

    #050

--- 050. Pow(x, n)/solution.cpp
+++ 050. Pow(x, n)/solution.cpp
@@ -10,11 +10,23 @@ using namespace std;
 class Solution {
 public:
     double myPow(double x, int n) {
-        
+        bool neg = n < 0;
+        long long ln = neg ? -(long long)n : n;
+        double res = 1;
+        while (ln) {
+            if (ln & 1) res *= x;
+            x *= x;
+            ln >>= 1;
+        }
+        return neg ? 1 / res : res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.myPow(2, 3) == 8;
+    ASSERT s.myPow(2, 10) == 1024;
+    ASSERT s.myPow(2, -1) == 0.5;
+    ASSERT s.myPow(1, -2147483648) == 1;
     return 0;
 }
