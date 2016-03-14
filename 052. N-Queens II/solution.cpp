91c5180966a9f51ad4f334c52aca6bd93ae0e523
zlsun <zlsun1995@gmail.com>
2016-03-14 22:25:41 +0800

    #052

--- 052. N-Queens II/solution.cpp
+++ 052. N-Queens II/solution.cpp
@@ -10,13 +10,34 @@ Now, instead outputting board configurations, return the total number of distinc
 using namespace std;
 
 class Solution {
+private:
+    int fullrow;
+    int ans;
 public:
     int totalNQueens(int n) {
-        
+        ans = 0;
+        fullrow = (1 << n) - 1;
+        search(0, 0, 0, 0);
+        return ans;
+    }
+    void search(int idx, int row, int ld, int rd) {
+        int pos, p;
+        if (row == fullrow) {
+            ++ans;
+        } else {
+            pos = fullrow & (~(row | ld | rd));
+            while (pos) {
+                p = pos & -pos;
+                pos -= p;
+                search(idx + 1, row | p, (ld | p) << 1, (rd | p) >> 1);
+            }
+        }
     }
 };
 
+
 int main() {
     Solution s;
+    ASSERT s.totalNQueens(4) == 2;
     return 0;
 }
