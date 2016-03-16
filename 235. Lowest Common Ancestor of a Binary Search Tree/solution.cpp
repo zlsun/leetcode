f48faa79b296197ca004868b0c5d06697372b787
zlsun <zlsun1995@gmail.com>
2016-03-17 00:06:59 +0800

    #235

--- 235. Lowest Common Ancestor of a Binary Search Tree/solution.cpp
+++ 235. Lowest Common Ancestor of a Binary Search Tree/solution.cpp
@@ -31,7 +31,13 @@ using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if (p->val == root->val) return p;
+        if (q->val == root->val) return q;
+        if (p->val < root->val && root->val < q->val) return root;
+        if (q->val < root->val && root->val < p->val) return root;
+        if (root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+        if (root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        return root;
     }
 };
 
