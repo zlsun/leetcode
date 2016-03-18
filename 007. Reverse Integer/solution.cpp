5cc213d22f729a7690adaf0387611a001a0204b3
zlsun <zlsun1995@gmail.com>
2016-03-18 16:56:22 +0800

    #007

--- 007. Reverse Integer/solution.cpp
+++ 007. Reverse Integer/solution.cpp
@@ -25,14 +25,29 @@ Test cases had been added to test the overflow behavior.
 #include "../utils.h"
 using namespace std;
 
+#include <cstdint>
 class Solution {
 public:
     int reverse(int x) {
-        
+        if (x == INT_MIN) return 0;
+        bool neg = x < 0;
+        x = neg ? -x : x;
+        int r = 0;
+        while (x) {
+            if (r > INT_MAX / 10) return 0;
+            r *= 10;
+            r += x % 10;
+            x /= 10;
+        }
+        return neg ? -r : r;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.reverse(123) == 321;
+    ASSERT s.reverse(-123) == -321;
+    ASSERT s.reverse(1534236469) == 0;
+    ASSERT s.reverse(-2147483648) == 0;
     return 0;
 }
