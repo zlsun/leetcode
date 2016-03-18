861d8e0f308f7f129b01370e787e4ceb38857b6a
zlsun <zlsun1995@gmail.com>
2016-03-18 13:27:40 +0800

    #263

--- 263. Ugly Number/solution.cpp
+++ 263. Ugly Number/solution.cpp
@@ -16,11 +16,21 @@ using namespace std;
 class Solution {
 public:
     bool isUgly(int num) {
-        
+        if (num <= 0) return false;
+        int factor[] = {2, 3, 5};
+        for (int i = 0; i < 3; ++i) {
+            int f = factor[i];
+            while (num % f == 0) num /= f;
+        }
+        return num == 1;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT !s.isUgly(0);
+    ASSERT s.isUgly(6);
+    ASSERT s.isUgly(8);
+    ASSERT !s.isUgly(14);
     return 0;
 }
