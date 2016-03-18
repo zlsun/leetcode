81434b1b21e3b3af3d1c6a964fd0c635c2275f21
zlsun <zlsun1995@gmail.com>
2016-03-19 01:10:53 +0800

    #200

--- 200. Number of Islands/solution.cpp
+++ 200. Number of Islands/solution.cpp
@@ -17,12 +17,38 @@ using namespace std;
 
 class Solution {
 public:
+    int n, m;
     int numIslands(vector<vector<char>>& grid) {
-        
+        n = grid.size();
+        if (n == 0) return 0;
+        m = grid[0].size();
+        if (m == 0) return 0;
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    ++ans;
+                }
+            }
+        return ans;
+    }
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= n) return;
+        if (j < 0 || j >= m) return;
+        if (grid[i][j] == '1') {
+            grid[i][j] = '2';
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+        }
     }
 };
 
 int main() {
     Solution s;
+    vector<vector<char>> m {{'1', '1'}};
+    ASSERT s.numIslands(m) == 1;
     return 0;
 }
