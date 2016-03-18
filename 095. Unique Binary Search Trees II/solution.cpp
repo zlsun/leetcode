04508ce8862fbc0c1ca1e8f3ee8bfb5b00c7a4c9
zlsun <zlsun1995@gmail.com>
2016-03-18 22:31:57 +0800

    #095

--- 095. Unique Binary Search Trees II/solution.cpp
+++ 095. Unique Binary Search Trees II/solution.cpp
@@ -26,7 +26,7 @@ Here's an example:
     \
      5
 
-The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
 
 **/
 
@@ -46,7 +46,29 @@ using namespace std;
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
-        
+        if (n == 0) return vector<TreeNode*>();
+        return generate(1, n);
+    }
+    vector<TreeNode*> generate(int b, int e) {
+        vector<TreeNode*> res;
+        if (b > e) res.push_back(NULL);
+        else if (b == e) res.push_back(new TreeNode(b));
+        else {
+            for (int k = b; k <= e; ++k) {
+                vector<TreeNode*> ls = generate(b, k-1);
+                vector<TreeNode*> rs = generate(k+1, e);
+                int ll = ls.size(), rl = rs.size();
+                for (int i = 0; i < ll; ++i) {
+                    for (int j = 0; j < rl; ++j) {
+                        TreeNode* node = new TreeNode(k);
+                        node->left = ls[i];
+                        node->right = rs[j];
+                        res.push_back(node);
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
 
