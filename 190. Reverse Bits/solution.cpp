2c26f7929f675b06d6875aa8dd00e7decf1d1a18
zlsun <zlsun1995@gmail.com>
2016-03-18 13:02:02 +0800

    #190

--- 190. Reverse Bits/solution.cpp
+++ 190. Reverse Bits/solution.cpp
@@ -18,11 +18,18 @@ using namespace std;
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        
+        uint32_t r = 0;
+        for (int i = 0; i < 32; ++i) {
+            r <<= 1;
+            if (n & 1) r |= 1;
+            n >>= 1;
+        }
+        return r;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.reverseBits(43261596) == 964176192;
     return 0;
 }
