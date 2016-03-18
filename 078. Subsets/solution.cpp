d0ed3ef01af78014c0d86e02b92af56cfdeac7a1
zlsun <zlsun1995@gmail.com>
2016-03-18 22:31:51 +0800

    #078

--- 078. Subsets/solution.cpp
+++ 078. Subsets/solution.cpp
@@ -27,14 +27,38 @@ If nums = [1,2,3], a solution is:
 #include "../utils.h"
 using namespace std;
 
+#include <algorithm>
+#include <iterator>
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+        vector<vector<int>> res;
+        res.push_back(vector<int>());
+        sort(nums.begin(), nums.end());
+        for (auto n : nums) {
+            vector<vector<int>> v;
+            for (auto r : res) {
+                r.push_back(n);
+                v.push_back(r);
+            }
+            copy(v.begin(), v.end(), std::back_inserter(res));
+        }
+        return res;
     }
 };
 
 int main() {
     Solution s;
+    vi v{1, 2, 3};
+    ASSERT s.subsets(v) == vvi {
+        {},
+        {1},
+        {2},
+        {1, 2},
+        {3},
+        {1, 3},
+        {2, 3},
+        {1, 2, 3}
+    };
     return 0;
 }
