39b08bd98d7feece4c066e2cd3daf4397fff48b3
zlsun <zlsun1995@gmail.com>
2016-03-18 22:32:02 +0800

    #096

--- 096. Unique Binary Search Trees/solution.cpp
+++ 096. Unique Binary Search Trees/solution.cpp
@@ -19,11 +19,19 @@ using namespace std;
 class Solution {
 public:
     int numTrees(int n) {
-        
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int k = 1; k <= i; ++k) {
+                dp[i] += dp[k - 1] * dp[i - k];
+            }
+        }
+        return dp[n];
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.numTrees(3) == 5;
     return 0;
 }
