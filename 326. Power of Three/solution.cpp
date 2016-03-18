eead73285a097f5d13057d3bb619e9baad6b3e99
zlsun <zlsun1995@gmail.com>
2016-03-18 13:02:18 +0800

    #326

--- 326. Power of Three/solution.cpp
+++ 326. Power of Three/solution.cpp
@@ -15,11 +15,14 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
+        return n > 0 && (1162261467 % n == 0);
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.isPowerOfThree(3);
+    ASSERT s.isPowerOfThree(27);
+    ASSERT !s.isPowerOfThree(28);
     return 0;
 }
