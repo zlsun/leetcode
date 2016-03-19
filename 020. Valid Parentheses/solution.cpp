1d5a086224a45d47fb9cfeea171148036b664aea
zlsun <zlsun1995@gmail.com>
2016-03-19 15:07:46 +0800

    #020

--- 020. Valid Parentheses/solution.cpp
+++ 020. Valid Parentheses/solution.cpp
@@ -9,14 +9,31 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
 #include "../utils.h"
 using namespace std;
 
+#include <stack>
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char> S;
+        map<char, char> par {{')', '('}, {']', '['}, {'}', '{'}};
+        for (auto c : s) {
+            switch (c) {
+                case '(': case '[': case '{':
+                    S.push(c);
+                    break;
+                case ')': case ']': case '}':
+                    if (S.empty() || S.top() != par[c]) return false;
+                    S.pop();
+            }
+        }
+        return S.empty();
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.isValid("([])");
+    ASSERT s.isValid("([]()){}");
+    ASSERT !s.isValid("([](){}");
+    ASSERT !s.isValid("[])");
     return 0;
 }
