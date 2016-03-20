7d8e0ea4ca26edca53b4208931ec0f5becc84109
zlsun <zlsun1995@gmail.com>
2016-03-21 00:14:43 +0800

    #071

--- 071. Simplify Path/solution.cpp
+++ 071. Simplify Path/solution.cpp
@@ -23,11 +23,37 @@ using namespace std;
 class Solution {
 public:
     string simplifyPath(string path) {
-        
+        if (path.back() != '/') path += '/';
+        vector<string> ps;
+        size_t b = 1, e;
+        while ((e = path.find('/', b)) != string::npos) {
+            if (e > b) ps.push_back(path.substr(b, e - b));
+            b = e + 1;
+        }
+        vector<string> s;
+        for (auto& p : ps) {
+            if (p == ".") continue;
+            else if (p == "..") {
+                if (s.size()) s.pop_back();
+            } else s.push_back(p);
+        }
+        string res;
+        for (auto& p : s) {
+            res += '/' + p;
+        }
+        return res.empty() ? "/" : res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.simplifyPath("/home/") == "/home";
+    ASSERT s.simplifyPath("/a/./b/../../c/") == "/c";
+    ASSERT s.simplifyPath("//") == "/";
+    ASSERT s.simplifyPath("///") == "/";
+    ASSERT s.simplifyPath("/./") == "/";
+    ASSERT s.simplifyPath("/../") == "/";
+    ASSERT s.simplifyPath("/...") == "/...";
+    ASSERT s.simplifyPath("/a/./b///../c/../././../d/..//../e/./f/./g/././//.//h///././/..///") == "/e/f/g";
     return 0;
 }
