20eb6874e0babbc8466f30170edde0a4803e7eb2
zlsun <zlsun1995@gmail.com>
2016-03-20 15:25:47 +0800

    #063

--- 063. Unique Paths II/solution.cpp
+++ 063. Unique Paths II/solution.cpp
@@ -25,12 +25,37 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
+    int uniquePathsWithObstacles(const vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size(), m = n ? obstacleGrid[0].size() : 0;
+        vector<int> dp(m + 1);
+        int v = 1;
+        for (int j = 0; j < m; ++j) {
+            if (!obstacleGrid[0][j]) {
+                dp[j + 1] = v;
+            } else {
+                v = 0;
+            }
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (!obstacleGrid[i][j - 1]) {
+                    if (!obstacleGrid[i][j - 2])
+                        dp[j] += dp[j - 1];
+                } else {
+                    dp[j] = 0;
+                }
+            }
+        }
+        return dp[m];
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.uniquePathsWithObstacles({
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 0, 0}
+    }) == 2;
     return 0;
 }
