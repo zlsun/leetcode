49bcee1c7bb5e3df7c80289b8206e727c604256f
zlsun <zlsun1995@gmail.com>
2016-03-20 17:25:33 +0800

    #066

--- 066. Plus One/solution.cpp
+++ 066. Plus One/solution.cpp
@@ -10,12 +10,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        
+    vector<int> plusOne(const vector<int>& digits) {
+        vector<int> res(digits);
+        int l = digits.size();
+        int inc = 1;
+        for (int i = l - 1; inc && i >= 0; --i) {
+            res[i] += inc;
+            inc = res[i] / 10;
+            res[i] %= 10;
+        }
+        if (inc) res.insert(res.begin(), inc);
+        return res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.plusOne({9}) == vi{1, 0};
     return 0;
 }
