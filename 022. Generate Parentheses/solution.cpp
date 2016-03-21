2607cfd8162f111099fdc2f176e536ce9c4adacb
zlsun <zlsun1995@gmail.com>
2016-03-22 00:34:24 +0800

    #022

--- 022. Generate Parentheses/solution.cpp
+++ 022. Generate Parentheses/solution.cpp
@@ -14,12 +14,26 @@ using namespace std;
 
 class Solution {
 public:
+    void dfs(vector<string>& res, string par, int l, int r) {
+        if (l < 0 || l > r) return;
+        if (r == 0) {
+            res.push_back(par);
+            return;
+        }
+        dfs(res, par + "(", l - 1, r);
+        dfs(res, par + ")", l, r - 1);
+    }
     vector<string> generateParenthesis(int n) {
-        
+        vector<string> res;
+        dfs(res, "", n, n);
+        return res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.generateParenthesis(3) == vector<string> {
+        "((()))", "(()())", "(())()", "()(())", "()()()"
+    };
     return 0;
 }
