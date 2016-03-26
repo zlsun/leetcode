1cf053dadbfaa63ed30012c4cac21d51e51e9594
zlsun <zlsun1995@gmail.com>
2016-03-26 15:40:36 +0800

    #047

--- 047. Permutations II/solution.cpp
+++ 047. Permutations II/solution.cpp
@@ -14,12 +14,31 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+    vector<vector<int>> permuteUnique(const vector<int>& nums) {
+        vector<vector<int>> res;
+        dfs(res, nums, vector<int>());
+        return res;
+    }
+    void dfs(vector<vector<int>>& res, vector<int> nums, vector<int> pre) {
+        if (nums.size() == 0) {
+            res.push_back(pre);
+            return;
+        }
+        set<int> ns(nums.begin(), nums.end());
+        for (int i : ns) {
+            vector<int> s(nums);
+            s.erase(find(s.begin(), s.end(), i));
+            vector<int> p(pre);
+            p.push_back(i);
+            dfs(res, s, p);
+        }
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.permuteUnique({1, 1, 2}) == vvi {
+        {1, 1, 2}, {1, 2, 1}, {2, 1, 1}
+    };
     return 0;
 }
