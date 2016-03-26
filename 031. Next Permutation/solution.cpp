09fb12a485bbc2c7333ecbb789cdd1f00cae85f2
zlsun <zlsun1995@gmail.com>
2016-03-26 15:40:17 +0800

    #031

--- 031. Next Permutation/solution.cpp
+++ 031. Next Permutation/solution.cpp
@@ -7,9 +7,9 @@ If such arrangement is not possible, it must rearrange it as the lowest possible
 The replacement must be in-place, do not allocate extra memory.
 
 Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
-1,2,3 &#8594; 1,3,2
-3,2,1 &#8594; 1,2,3
-1,1,5 &#8594; 1,5,1
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
 
 **/
 
@@ -20,11 +20,24 @@ using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
+        int i, j;
+        i = j = nums.size() - 1;
+        while (i > 0 && nums[i] <= nums[i - 1]) --i;
+        if (i == 0) return reverse(nums.begin(), nums.end());
+        while (nums[j] <= nums[i - 1]) --j;
+        swap(nums[j], nums[i - 1]);
+        reverse(nums.begin() + i, nums.end());
     }
 };
 
 int main() {
     Solution s;
+    vi v {1, 2, 3};
+    s.nextPermutation(v);
+    ASSERT v == vi{1, 3, 2};
+    s.nextPermutation(v);
+    ASSERT v == vi{2, 1, 3};
+    s.nextPermutation(v);
+    ASSERT v == vi{2, 3, 1};
     return 0;
 }
