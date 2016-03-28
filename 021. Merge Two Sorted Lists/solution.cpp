50917752c8e756d58bdd55b1f8cc088ab0dea5d9
zlsun <zlsun1995@gmail.com>
2016-03-28 11:32:49 +0800

    #021

--- 021. Merge Two Sorted Lists/solution.cpp
+++ 021. Merge Two Sorted Lists/solution.cpp
@@ -17,11 +17,36 @@ using namespace std;
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+        if (l1 == nullptr) return l2;
+        if (l2 == nullptr) return l1;
+        ListNode* r;
+        if (l1->val < l2->val) {
+            r = l1;
+            l1 = l1->next;
+        } else {
+            r = l2;
+            l2 = l2->next;
+        }
+        ListNode* p = r;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if (l1) p->next = l1;
+        if (l2) p->next = l2;
+        return r;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.mergeTwoLists(ListNode::Builder{1, 3, 5}, ListNode::Builder{2, 4, 6})->equal(ListNode::Builder{1, 2, 3, 4, 5, 6});
+    ASSERT s.mergeTwoLists(ListNode::Builder{2}, ListNode::Builder{1})->equal(ListNode::Builder{1, 2});
     return 0;
 }
