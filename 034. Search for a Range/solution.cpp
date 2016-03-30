c3685c5d2d246bd452a87160a7e182f1c983edf1
zlsun <zlsun1995@gmail.com>
2016-03-30 15:50:10 +0800

    #034

--- 034. Search for a Range/solution.cpp
+++ 034. Search for a Range/solution.cpp
@@ -17,12 +17,43 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
+    vector<int> searchRange(const vector<int>& nums, int target) {
+        vector<int> res {
+            lower_bound(nums, target),
+            upper_bound(nums, target)
+        };
+        return res;
+    }
+    int lower_bound(const vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return nums[l] == target ? l : (nums[r] == target ? r : -1);
+    }
+    int upper_bound(const vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > target) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        return nums[r] == target ? r : (nums[l] == target ? l : -1);
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.searchRange({5, 7, 7, 8, 8, 10}, 8) == vi {3, 4};
+    ASSERT s.searchRange({1}, 0) == vi {-1, -1};
+    ASSERT s.searchRange({1}, 1) == vi {0, 0};
     return 0;
 }
