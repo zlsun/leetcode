ec19a9fa00399ac3de8effc6d2c2b7dd51b3521b
zlsun <zlsun1995@gmail.com>
2016-03-30 15:50:05 +0800

    #024

--- 024. Swap Nodes in Pairs/solution.cpp
+++ 024. Swap Nodes in Pairs/solution.cpp
@@ -24,11 +24,29 @@ using namespace std;
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
+        if (head == nullptr || head->next == nullptr) return head;
+        auto pre = head;
+        head = head->next;
+        pre->next = head->next;
+        head->next = pre;
+        while (pre->next && pre->next->next) {
+            auto p = pre->next;
+            auto q = p->next;
+            pre->next = q;
+            p->next = q->next;
+            q->next = p;
+            pre = p;
+        }
+        return head;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.swapPairs(ListNode::Builder{})->equal(ListNode::Builder{});
+    ASSERT s.swapPairs(ListNode::Builder{1})->equal(ListNode::Builder{1});
+    ASSERT s.swapPairs(ListNode::Builder{1, 2})->equal(ListNode::Builder{2, 1});
+    ASSERT s.swapPairs(ListNode::Builder{1, 2, 3, 4})->equal(ListNode::Builder{2, 1, 4, 3});
+    ASSERT s.swapPairs(ListNode::Builder{1, 2, 3, 4, 5})->equal(ListNode::Builder{2, 1, 4, 3, 5});
     return 0;
 }
