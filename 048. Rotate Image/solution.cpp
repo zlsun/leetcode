fe9e2c543b2875dd21c44851506013094be7e78d
zlsun <zlsun1995@gmail.com>
2016-03-30 15:50:14 +0800

    #048

--- 048. Rotate Image/solution.cpp
+++ 048. Rotate Image/solution.cpp
@@ -12,11 +12,42 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int ii = n - i - 1;
+                int jj = n - j - 1;
+                int tmp = matrix[jj][i];
+                matrix[jj][i] = matrix[ii][jj];
+                matrix[ii][jj] = matrix[j][ii];
+                matrix[j][ii] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
     }
 };
 
 int main() {
     Solution s;
+    vvi m {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    s.rotate(m);
+    ASSERT m == vvi {
+        {7, 4, 1},
+        {8, 5, 2},
+        {9, 6, 3}
+    };
+    m = vvi {
+        {1, 2},
+        {3, 4}
+    };
+    s.rotate(m);
+    ASSERT m == vvi {
+        {3, 1},
+        {4, 2}
+    };
     return 0;
 }
