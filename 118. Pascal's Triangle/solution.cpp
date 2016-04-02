b7014d7b59f8a4ca1ab46c7a6a8be1a6789b730a
zlsun <zlsun1995@gmail.com>
2016-04-03 00:13:24 +0800

    #118

--- 118. Pascal's Triangle/solution.cpp
+++ 118. Pascal's Triangle/solution.cpp
@@ -20,12 +20,28 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> m(n);
+        for (int i = 0; i < n; ++i) {
+            m[i].resize(i + 1);
+            m[i][0] = m[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                m[i][j] = m[i - 1][j - 1] + m[i - 1][j];
+            }
+        }
+        return m;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.generate(1) == vvi {{1}};
+    ASSERT s.generate(5) == vvi {
+        {1},
+        {1, 1},
+        {1, 2, 1},
+        {1, 3, 3, 1},
+        {1, 4, 6, 4, 1}
+    };
     return 0;
 }
