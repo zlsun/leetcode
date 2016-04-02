5b39f2584a544e8e8eb46c2ba400637db989fead
zlsun <zlsun1995@gmail.com>
2016-04-02 23:46:05 +0800

    #074

--- 074. Search a 2D Matrix/solution.cpp
+++ 074. Search a 2D Matrix/solution.cpp
@@ -24,11 +24,34 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        int m = matrix.size(), n = m ? matrix[0].size() : 0;
+        if (m == 0 || n == 0) return false;
+        vector<int> col;
+        for (auto& v : matrix) col.push_back(v[0]);
+        int l = lower_bound(col.begin(), col.end(), target) - col.begin();
+        l = max(0, l - 1);
+        int u = upper_bound(col.begin(), col.end(), target) - col.begin();
+        for (int i = l; i < u; ++i) {
+            auto& row = matrix[i];
+            if (binary_search(row.begin(), row.end(), target)) {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
 int main() {
     Solution s;
+    vvi m {
+        {1,   3,  5,  7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 50}
+    };
+    ASSERT s.searchMatrix(m, 1);
+    ASSERT s.searchMatrix(m, 3);
+    ASSERT s.searchMatrix(m, 7);
+    ASSERT s.searchMatrix(m, 10);
+    ASSERT !s.searchMatrix(m, 12);
     return 0;
 }
