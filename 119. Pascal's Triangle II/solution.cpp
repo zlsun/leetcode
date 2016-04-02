b54e723ba37a0aa6396125bd48c845c638365c1b
zlsun <zlsun1995@gmail.com>
2016-04-03 00:14:32 +0800

    #119

--- 119. Pascal's Triangle II/solution.cpp
+++ 119. Pascal's Triangle II/solution.cpp
@@ -16,11 +16,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        
+        vector<int> r1(rowIndex + 1), r2(rowIndex + 1);
+        for (int i = 0; i <= rowIndex; ++i) {
+            r2[0] = r2[i] = 1;
+            for (int j = 1; j < i; ++j) {
+                r2[j] = r1[j - 1] + r1[j];
+            }
+            swap(r1, r2);
+        }
+        return r1;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.getRow(0) == vi {1};
+    ASSERT s.getRow(1) == vi {1, 1};
+    ASSERT s.getRow(2) == vi {1, 2, 1};
+    ASSERT s.getRow(3) == vi {1, 3, 3, 1};
+    ASSERT s.getRow(4) == vi {1, 4, 6, 4, 1};
+    // ASSERT s.getRow(5) == vi {1, 5, 10, 10, 5, 1};
     return 0;
 }
