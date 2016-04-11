3bc0ee55d2f99e6f2a3b50183837f7143d7964bd
zlsun <zlsun1995@gmail.com>
2016-04-11 23:56:38 +0800

    #077

--- 077. Combinations/solution.cpp
+++ 077. Combinations/solution.cpp
@@ -23,11 +23,33 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-        
+        vector<vector<int>> res;
+        vector<int> v;
+        dfs(res, n, k, 1, v);
+        return res;
+    }
+    void dfs(vector<vector<int>>& res, int n, int k, int from, vector<int>& v) {
+        if (k == 0) {
+            res.push_back(v);
+            return;
+        }
+        for (int i = from; i <= n; ++i) {
+            v.push_back(i);
+            dfs(res, n, k - 1, i + 1, v);
+            v.pop_back();
+        }
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.combine(4, 2) == vvi {
+        {1, 2},
+        {1, 3},
+        {1, 4},
+        {2, 3},
+        {2, 4},
+        {3, 4},
+    };
     return 0;
 }
