37af8cdef238bac67de0cdd11f168071052330d9
zlsun <zlsun1995@gmail.com>
2016-04-18 23:47:56 +0800

    #342

--- 342. Power of Four/solution.cpp
+++ 342. Power of Four/solution.cpp
@@ -18,11 +18,15 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        
+        return num > 0 && !(num & (num - 1)) && !(num & 0xAAAAAAAA);
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.isPowerOfFour(1);
+    ASSERT s.isPowerOfFour(4);
+    ASSERT !s.isPowerOfFour(5);
+    ASSERT s.isPowerOfFour(16);
     return 0;
 }
