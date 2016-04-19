d395be4b0228b03055b0ede99483e81e56156992
zlsun <zlsun1995@gmail.com>
2016-04-19 22:55:34 +0800

    #029

--- 029. Divide Two Integers/solution.cpp
+++ 029. Divide Two Integers/solution.cpp
@@ -13,11 +13,28 @@ using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        
+        if (!divisor || (dividend == INT_MIN && divisor == -1)) return INT_MAX;
+        long m = labs(dividend), n = labs(divisor);
+        int res = 0;
+        while (m >= n) {
+            long long t = n, count = 1;
+            while (m >= (t << 1)) {
+                t <<= 1;
+                count <<= 1;
+            }
+            m -= t;
+            res += count;
+        }
+        int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+        return sign * res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.divide(16, 4) == 4;
+    ASSERT s.divide(INT_MAX, 4) == INT_MAX / 4;
+    ASSERT s.divide(INT_MIN, -1) == INT_MAX;
+    ASSERT s.divide(INT_MAX, -1) == -INT_MAX;
     return 0;
 }
