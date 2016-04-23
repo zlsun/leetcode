eaaf2e1823d91a16805cb493c882bdf74f2d430e
zlsun <zlsun1995@gmail.com>
2016-04-23 12:30:38 +0800

    Add #344

---
+++ 344. Reverse String/solution.cpp
@@ -0,0 +1,23 @@
+/** 344. Reverse String
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+**/
+
+#include <iostream>
+#include "../utils.h"
+using namespace std;
+
+class Solution {
+public:
+    string reverseString(string s) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
