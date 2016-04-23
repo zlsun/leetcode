d7975408616cdb38a9f5f9bf19e351762e93b3d8
zlsun <zlsun1995@gmail.com>
2016-04-23 16:16:36 +0800

    Add #345

---
+++ 345. Reverse Vowels of a String/solution.cpp
@@ -0,0 +1,26 @@
+/** 345. Reverse Vowels of a String
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+**/
+
+#include <iostream>
+#include "../utils.h"
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
