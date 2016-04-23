4d941c130d62a476da2fbb4a404eed96b6549e7f
zlsun <zlsun1995@gmail.com>
2016-04-23 16:29:33 +0800

    #345

--- 345. Reverse Vowels of a String/solution.cpp
+++ 345. Reverse Vowels of a String/solution.cpp
@@ -15,12 +15,30 @@ using namespace std;
 
 class Solution {
 public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
     string reverseVowels(string s) {
-        
+        auto b = s.begin();
+        auto rb = s.rbegin();
+        string res;
+        for (; b != s.end(); ++b) {
+            if (isVowel(*b)) {
+                while (!isVowel(*rb)) ++rb;
+                res += *rb++;
+            } else {
+                res += *b;
+            }
+        }
+        return res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.reverseVowels("aA") == "Aa";
+    ASSERT s.reverseVowels("hello") == "holle";
+    ASSERT s.reverseVowels("leetcode") == "leotcede";
     return 0;
 }
