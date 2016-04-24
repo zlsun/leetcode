a1812b12092ed22d0af05cc5c8fdd72b0748c91e
zlsun <zlsun1995@gmail.com>
2016-04-24 12:43:11 +0800

    #064

--- 064. Minimum Path Sum/solution.cpp
+++ 064. Minimum Path Sum/solution.cpp
@@ -10,12 +10,24 @@ using namespace std;
 
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        
+    int minPathSum(const vector<vector<int>>& grid) {
+        int m = grid.size(), n = m ? grid[0].size() : 0;
+        vector<int> dp(n + 1, 0x3f3f3f3f);
+        dp[1] = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[j] = min(dp[j - 1], dp[j]) + grid[i][j - 1];
+            }
+        }
+        return dp[n];
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.minPathSum(VVi {
+        {1, 1, 1},
+        {1, 2, 1},
+    }) == 4;
     return 0;
 }
