7de161f405a8b334803d1bb89b31896657ca29d7
zlsun <zlsun1995@gmail.com>
2016-05-21 21:34:14 +0800

    Add 347, 349, 350

---
+++ 349. Intersection of Two Arrays/solution.cpp
@@ -0,0 +1,29 @@
+/** 349. Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+
+**/
+
+#include <iostream>
+#include "../utils.h"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
