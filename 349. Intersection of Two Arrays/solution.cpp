cd9bc20bbee33b92b65af84aef76498ecaaac0dc
zlsun <zlsun1995@gmail.com>
2016-05-22 11:18:55 +0800

    #349

--- 349. Intersection of Two Arrays/solution.cpp
+++ 349. Intersection of Two Arrays/solution.cpp
@@ -18,12 +18,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
+    vector<int> intersection(const vector<int>& nums1, const vector<int>& nums2) {
+        vector<int> v;
+        set<int> s1(nums1.begin(), nums1.end()), s2(nums2.begin(), nums2.end());
+        set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(v));
+        return v;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.intersection(vi{1, 2, 2, 1}, vi{2, 2}) == vi{2};
     return 0;
 }
