1096c7bac3e5a027c2e855699b4c355e47c4867b
zlsun <zlsun1995@gmail.com>
2016-05-22 11:32:44 +0800

    #347

--- 347. Top K Frequent Elements/solution.cpp
+++ 347. Top K Frequent Elements/solution.cpp
@@ -5,7 +5,7 @@ Given a non-empty array of integers, return the k most frequent elements.
 For example,
 Given [1,1,1,2,2,3] and k = 2, return [1,2].
 
-Note: 
+Note:
 
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
 Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
@@ -18,12 +18,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+    vector<int> topKFrequent(const vector<int>& nums, int k) {
+        unordered_map<int, int> counter;
+        for (auto i : nums) counter[i]++;
+        vector<pair<int, int>> v(counter.begin(), counter.end());
+        nth_element(v.begin(), v.begin() + k, v.end(), [](auto a, auto b) { return a.second > b.second; });
+        vector<int> res;
+        transform(v.begin(), v.begin() + k, back_inserter(res), [](auto p) { return p.first; });
+        return res;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.topKFrequent(vi{1, 1, 1, 2, 2, 3}, 2) == vi{1, 2};
     return 0;
 }
