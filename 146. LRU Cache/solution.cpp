4d9cc55815c1316355eed8f4e1df9782d04f9e47
zlsun <zlsun1995@gmail.com>
2016-05-24 15:33:42 +0800

    #146

--- 146. LRU Cache/solution.cpp
+++ 146. LRU Cache/solution.cpp
@@ -11,22 +11,48 @@ set(key, value) - Set or insert the value if the key is not already present. Whe
 #include "../utils.h"
 using namespace std;
 
-class LRUCache{
+class LRUCache {
+private:
+    int capacity;
+    list<int> recent;
+    unordered_map<int, int> cache;
+    unordered_map<int, list<int>::iterator> pos;
+    void use(int key) {
+        if (pos.find(key) != pos.end()) {
+            recent.erase(pos[key]);
+        } else if (recent.size() >= capacity) {
+            int old = recent.back();
+            recent.pop_back();
+            cache.erase(old);
+            pos.erase(old);
+        }
+        recent.push_front(key);
+        pos[key] = recent.begin();
+    }
 public:
-    LRUCache(int capacity) {
-        
-    }
-    
+    LRUCache(int capacity): capacity(capacity) {}
     int get(int key) {
-        
+        if (cache.find(key) != cache.end()) {
+            use(key);
+            return cache[key];
+        }
+        return -1;
     }
-    
     void set(int key, int value) {
-        
+        use(key);
+        cache[key] = value;
     }
 };
 
 int main() {
-    Solution s;
+    LRUCache c(2);
+    c.set(1, 1);
+    c.set(2, 2);
+    ASSERT c.get(2) == 2;
+    ASSERT c.get(1) == 1;
+    c.set(3, 3);
+    ASSERT c.get(1) == 1;
+    ASSERT c.get(2) == -1;
+    ASSERT c.get(3) == 3;
     return 0;
 }
