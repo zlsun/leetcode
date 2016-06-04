2b7e1b781ac9fb43c2a4da759bea78034b567cff
zlsun <zlsun1995@gmail.com>
2016-06-05 02:15:43 +0800

    #026

--- 026. Remove Duplicates from Sorted Array/solution.cpp
+++ 026. Remove Duplicates from Sorted Array/solution.cpp
@@ -18,11 +18,23 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int n = nums.size(), j = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || nums[i-1] != nums[i]) {
+                nums[j++] = nums[i];
+            }
+        }
+        if (j < n) nums.erase(nums.begin() + j, nums.end());
+        return j;
     }
 };
 
 int main() {
     Solution s;
+    vi a;
+    ASSERT s.removeDuplicates(a) == 0;
+    a = {1, 1, 2};
+    ASSERT s.removeDuplicates(a) == 2;
+    ASSERT a == vi{1, 2};
     return 0;
 }
