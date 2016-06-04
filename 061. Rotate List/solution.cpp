57edb1fb73c8c976bc4f74f53ac9c0f2e3508e08
zlsun <zlsun1995@gmail.com>
2016-06-05 01:59:22 +0800

    #061

--- 061. Rotate List/solution.cpp
+++ 061. Rotate List/solution.cpp
@@ -21,11 +21,25 @@ using namespace std;
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
+        int n = 0;
+        for (ListNode* p = head; p; p = p->next, ++n);
+        if (n == 0 || k % n == 0) return head;
+        k %= n;
+        ListNode* kth = getNth(head, n - k);
+        ListNode* pre = getNth(head, n - k - 1);
+        ListNode* end = getNth(head, n - 1);
+        pre->next = nullptr;
+        end->next = head;
+        return kth;
+    }
+    ListNode* getNth(ListNode* p, int n) {
+        for (; n; p = p->next, --n);
+        return p;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.rotateRight(ListNode::Builder{1, 2, 3, 4, 5}, 2)->equal(ListNode::Builder{4, 5, 1, 2, 3});
     return 0;
 }
