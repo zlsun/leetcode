073751ed9e72ac5b139e9e8d335092862e6bfcb8
zlsun <zlsun1995@gmail.com>
2016-06-05 02:36:20 +0800

    #172

--- 172. Factorial Trailing Zeroes/solution.cpp
+++ 172. Factorial Trailing Zeroes/solution.cpp
@@ -13,11 +13,13 @@ using namespace std;
 class Solution {
 public:
     int trailingZeroes(int n) {
-        
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.trailingZeroes(10) == 2;
+    ASSERT s.trailingZeroes(100) == 24;
     return 0;
 }
