74347f42007bd7513cd1217da56f8c09da78e15a
zlsun <zlsun1995@gmail.com>
2016-06-05 03:49:52 +0800

    #299

--- 299. Bulls and Cows/solution.cpp
+++ 299. Bulls and Cows/solution.cpp
@@ -8,7 +8,7 @@ Friend's guess: "7810"
 
 Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
 
-Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B". 
+Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
 
 Please note that both secret number and friend's guess may contain duplicate digits, for example:
 
@@ -29,11 +29,26 @@ using namespace std;
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        
+        int n = secret.length();
+        int a = 0, b = 0;
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; ++i) {
+            if (guess[i] == secret[i]) ++a;
+            else ++mp[secret[i]];
+        }
+        for (int i = 0; i < n; ++i) {
+            if (guess[i] != secret[i] && mp[guess[i]]) {
+                --mp[guess[i]];
+                ++b;
+            }
+        }
+        return to_string(a) + 'A' + to_string(b) + 'B';
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.getHint("1807", "7810") == "1A3B";
+    ASSERT s.getHint("1123", "0111") == "1A1B";
     return 0;
 }
