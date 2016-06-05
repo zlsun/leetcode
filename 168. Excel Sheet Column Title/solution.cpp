1b40754aeb77d92e848548046dc375ff6a44378c
zlsun <zlsun1995@gmail.com>
2016-06-05 15:24:26 +0800

    #168

--- 168. Excel Sheet Column Title/solution.cpp
+++ 168. Excel Sheet Column Title/solution.cpp
@@ -9,7 +9,7 @@ For example:
     ...
     26 -> Z
     27 -> AA
-    28 -> AB 
+    28 -> AB
 
 Credits:Special thanks to @ifanchu for adding this problem and creating all test cases.
 **/
@@ -21,11 +21,30 @@ using namespace std;
 class Solution {
 public:
     string convertToTitle(int n) {
-        
+        string s;
+        while (n) {
+            if (n % 26) {
+                s += 'A' + n % 26 - 1;
+                n /= 26;
+            } else {
+                s += 'Z';
+                n = n / 26 - 1;
+            }
+        }
+        reverse(s.begin(), s.end());
+        return s;
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.convertToTitle(1) == "A";
+    ASSERT s.convertToTitle(2) == "B";
+    ASSERT s.convertToTitle(3) == "C";
+    ASSERT s.convertToTitle(26) == "Z";
+    ASSERT s.convertToTitle(27) == "AA";
+    ASSERT s.convertToTitle(28) == "AB";
+    ASSERT s.convertToTitle(52) == "AZ";
+    ASSERT s.convertToTitle(53) == "BA";
     return 0;
 }
