7e6384c30944b9a44541ba173df0b43b1caf1516
zlsun <zlsun1995@gmail.com>
2016-06-05 15:10:17 +0800

    #155

--- 155. Min Stack/solution.cpp
+++ 155. Min Stack/solution.cpp
@@ -17,25 +17,48 @@ getMin() -- Retrieve the minimum element in the stack.
 using namespace std;
 
 class MinStack {
+    stack<int> s, m;
 public:
     void push(int x) {
-        
+        s.push(x);
+        if (m.empty() || x <= m.top()) m.push(x);
     }
 
     void pop() {
-        
+        int x = s.top();
+        s.pop();
+        if (x == m.top()) m.pop();
     }
 
     int top() {
-        
+        return s.top();
     }
 
     int getMin() {
-        
+        return m.top();
     }
 };
 
 int main() {
-    Solution s;
+    MinStack s;
+    s.push(1);
+    ASSERT s.top() == 1;
+    ASSERT s.getMin() == 1;
+    s.push(2);
+    ASSERT s.top() == 2;
+    ASSERT s.getMin() == 1;
+    s.push(0);
+    ASSERT s.top() == 0;
+    ASSERT s.getMin() == 0;
+    s.push(0);
+    ASSERT s.top() == 0;
+    ASSERT s.getMin() == 0;
+    s.pop();
+    s.pop();
+    ASSERT s.top() == 2;
+    ASSERT s.getMin() == 1;
+    s.pop();
+    ASSERT s.top() == 1;
+    ASSERT s.getMin() == 1;
     return 0;
 }
