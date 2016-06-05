7fc1ce5610d59980b3147f98ff4b0b5b23affcd4
zlsun <zlsun1995@gmail.com>
2016-06-05 12:37:08 +0800

    #290

--- 290. Word Pattern/solution.cpp
+++ 290. Word Pattern/solution.cpp
@@ -22,11 +22,29 @@ using namespace std;
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
-        
+        unordered_map<char, string> p2s;
+        unordered_map<string, char> s2p;
+        istringstream iss(str);
+        string s;
+        for (char c : pattern) {
+            if (!(iss >> s)) return false;
+            if (p2s.find(c) == p2s.end() && s2p.find(s) == s2p.end()) {
+                p2s[c] = s;
+                s2p[s] = c;
+            } else if (p2s[c] != s && s2p[s] != c) {
+                return false;
+            }
+        }
+        return !(iss >> s);
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.wordPattern("abba", "dog cat cat dog");
+    ASSERT !s.wordPattern("abba", "dog cat cat fish");
+    ASSERT !s.wordPattern("aaaa", "dog cat cat dog");
+    ASSERT !s.wordPattern("abba", "dog dog dog dog");
+    ASSERT !s.wordPattern("aaa", "aa aa aa aa");
     return 0;
 }
--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 msg=`git status --short | sed -nr 's/ M "([0-9]{3}).*$/#\1/p' | paste -s -d','`
-[ -n $msg ] && (
+[[ -n "$msg" ]] && (
     git add .
     git commit -m "$msg"
     git push
