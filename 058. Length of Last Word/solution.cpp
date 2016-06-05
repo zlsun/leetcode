fbd28fa76442590aaa212aa12d663b800b923114
zlsun <zlsun1995@gmail.com>
2016-06-05 12:40:13 +0800

    #058

--- 058. Length of Last Word/solution.cpp
+++ 058. Length of Last Word/solution.cpp
@@ -5,7 +5,7 @@ If the last word does not exist, return 0.
 
 Note: A word is defined as a character sequence consists of non-space characters only.
 
-For example, 
+For example,
 Given s = "Hello World",
 return 5.
 
@@ -18,11 +18,18 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        istringstream iss(s);
+        s.clear();
+        while (iss >> s);
+        return s.length();
     }
 };
 
 int main() {
     Solution s;
+    ASSERT s.lengthOfLastWord("") == 0;
+    ASSERT s.lengthOfLastWord(" ") == 0;
+    ASSERT s.lengthOfLastWord("world") == 5;
+    ASSERT s.lengthOfLastWord("Hello world") == 5;
     return 0;
 }
