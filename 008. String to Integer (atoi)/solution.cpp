3009203ddae54ee1ee1cd2c5c39c93765c28b086
zlsun <zlsun1995@gmail.com>
2016-06-05 15:54:47 +0800

    #008

--- 008. String to Integer (atoi)/solution.cpp
+++ 008. String to Integer (atoi)/solution.cpp
@@ -3,8 +3,8 @@ Implement atoi to convert a string to an integer.
 
 Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
 
-Notes: 
-It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front. 
+Notes:
+It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
 
 Update (2015-02-10):
 The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
@@ -30,11 +30,39 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
-        
+        int i = 0, l = str.length();
+        long long res = 0;
+        bool pos = true;
+        bool skipping = true;
+        while (i < l) {
+            char c = str[i];
+            if (isdigit(c)) {
+                skipping = false;
+                res *= 10;
+                res += c - '0';
+                if (res > INT_MAX && pos) return INT_MAX;
+                else if (!pos && -res < INT_MIN) return INT_MIN;
+            } else if (c == '-' && skipping) {
+                skipping = false;
+                pos = !pos;
+            } else if (c == '+' && skipping) {
+                skipping = false;
+            } else if (c == ' ' && skipping) {
+            } else {
+                break;
+            }
+            ++i;
+        }
+        return pos ? res : -res;
     }
 };
 
 int main() {
     Solution s;
+    for (int i : vi{0, 1, 12, 1000, INT_MAX, INT_MIN}) {
+        ASSERT s.myAtoi(to_string(i)) == i;
+    }
+    ASSERT s.myAtoi("+-2") == 0;
+    ASSERT s.myAtoi("  -0012a42") == -12;
     return 0;
 }
