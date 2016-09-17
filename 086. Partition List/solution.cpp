065c339a0e66a347c057ff9fe464596946fcee12
zlsun <zlsun1995@gmail.com>
2016-09-18 02:04:51 +0800

    #086

--- 086. Partition List/solution.cpp
+++ 086. Partition List/solution.cpp
@@ -24,11 +24,27 @@ using namespace std;
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        
+        ListNode ls(-1), geq(-1);
+        ListNode* last_ls = &ls, *last_geq = &geq;
+        while (head) {
+            if (head->val < x) {
+                last_ls->next = head;
+                last_ls = last_ls->next;
+            } else {
+                last_geq->next = head;
+                last_geq = last_geq->next;
+            }
+            head = head->next;
+        }
+        last_geq->next = nullptr;
+        last_ls->next = geq.next;
+        return ls.next;
     }
 };
 
 int main() {
     Solution s;
+    using L = ListNode::Builder;
+    ASSERT s.partition(L{1, 4, 3, 2, 5, 2}, 3)->equal(L{1, 2, 2, 4, 3, 5});
     return 0;
 }
