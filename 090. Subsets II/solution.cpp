e3cf4d92b3a99aafbad114654565022f3ee4c83b
zlsun <zlsun1995@gmail.com>
2016-09-18 01:16:30 +0800

    #090

--- 090. Subsets II/solution.cpp
+++ 090. Subsets II/solution.cpp
@@ -25,14 +25,56 @@ If nums = [1,2,2], a solution is:
 #include "../utils.h"
 using namespace std;
 
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
+    vvi subsetsWithDup(vi& nums) {
+        sort(nums.begin(), nums.end());
+        vpii count;
+        int idx = -1;
+        int pre = INT_MAX;
+        for (auto i : nums) {
+            if (i != pre) {
+                idx++;
+                count.push_back(pii(i, 0));
+            }
+            count[idx].second++;
+            pre = i;
+        }
+        vvi res;
+        vi cur;
+        dfs(res, cur, count, 0);
+        return res;
+    }
+    void dfs(vvi& res, vi& cur, vpii& count, int idx) {
+        string ident(idx * 4, ' ');
+        if (idx == count.size()) {
+            res.push_back(cur);
+            return;
+        }
+        pii& p = count[idx];
+        int num = p.first, cnt = p.second;
+        for (int rep = 0; rep <= cnt; ++rep) {
+            for (int i = 0; i < rep; ++i) {
+                cur.push_back(num);
+            }
+            dfs(res, cur, count, idx + 1);
+            for (int i = 0; i < rep; ++i) {
+                cur.pop_back();
+            }
+        }
     }
 };
 
 int main() {
     Solution s;
+    vi v {1, 2, 2};
+    ASSERT s.subsetsWithDup(v) == vvi{{}, {2}, {2, 2}, {1}, {1, 2}, {1, 2, 2}};
+    v = {-1, 1, 2};
+    ASSERT s.subsetsWithDup(v) == vvi{{}, {2}, {1}, {1, 2}, {-1}, {-1, 2}, {-1, 1}, {-1, 1, 2}};
     return 0;
 }
